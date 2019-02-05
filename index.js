48a8fd7a46e51d7466ae7fde469063786be7f121
Elizabeth Thys <elizabeth.thys@gmail.com>
2019-02-05 15:44:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  var parsed = parseInt(n,10);
+  const lis = document.getElementsByClassName('ranked-list');
+  for (let i = 0; i < lis.length; i++) {
+  lis[i].textContent = (i + parsed).toString();
+}
+}
+
+function deepestChild(){
+  const lis = document.getElementById('grand-node').querySelectorAll('div');
+  for (let i = 0; i < lis.length-1; i++) {
+    var list = lis[i].querySelector('div');
+  }
+  return list;
+}
