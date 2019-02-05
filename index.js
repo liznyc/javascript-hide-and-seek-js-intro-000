642072ac4b2429855655249556fac0e64e862494
Elizabeth Thys <elizabeth.thys@gmail.com>
2019-02-05 15:28:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function increaseRankBy(n) {
 }
 
 function deepestChild(){
-  const lis = document.getElementById('grand-node')
+  const lis = document.getElementById('div#grand-node')
     .querySelectorAll('div');
   for (let i = 0; i < lis.length; i++) {
     return lis[i].innerHTML;
